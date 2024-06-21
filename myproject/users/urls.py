0c33684cb9d8d127ab9394f7c4a09662a6d35f94
kevin <kevinpurnomo06@gmail.com>
2024-06-22 06:46:32 +0700

    Commit

---
+++ myproject/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login_and_register, name='login'),
+    path('register/', views.login_and_register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.update_profile, name='profile'),
+    path('update-profile/', views.update_profile, name='update_profile'),
+]
