94a4e6aa21c4b522e9da15b23bb233cd885f8a32
kevin <kevinpurnomo06@gmail.com>
2024-06-28 22:23:24 +0700

    clean up settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,30 +20,20 @@ EMAIL_HOST_USER = 'kevinpurnomo06@gmail.com'
 EMAIL_HOST_PASSWORD = 'pets cffq yrnm gymb'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'django_login',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',   
-        'PORT': '3306',
-    }   
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'django_login',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',   
+#         'PORT': '3306',
+#     }   
+# }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# PPPPPPPPPPPPPPPPPPPP
-# TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-# STATIC_DIR = os.path.join(BASE_DIR,'static')
-# MEDIA_DIR = os.path.join(BASE_DIR,'media')
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [STATIC_DIR,]
-# MEDIA_ROOT = MEDIA_DIR
-# MEDIA_URL = '/media/'
-# LOGIN_URL = '/akun/user_login/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
